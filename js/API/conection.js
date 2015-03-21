5d92520786d14f9cee316822e3ffd4a38ffeef55
rebecaaguilar <rebeca_eugenia@yahoo.com>
2015-03-21 12:29:47 -0600

    HotelApp3

---
+++ js/API/conection.js
@@ -0,0 +1,10 @@
+//network information  valida si el dispositivo esta conectado a alguna red
+var conection = {
+	isConnected: function () {
+	   if (navigator.connection.type!=Connection.NONE) {
+	   	return true;
+	   else 
+	   	return false;
+	   }	
+	}
+};
--- js/actions.js
+++ js/actions.js
@@ -1,13 +1,17 @@
 var fn = {
+//=========================== READY ===================
     ready: function(){
-        document.addEventListener('deviceready',fn.init,false);
+        document.addEventListener('deviceready',fn.init,false);  //deviceready es solo para dispositivos
     },
+//=========================== INIT ===================    
     init: function(){
         if(!almacenamiento.isRegistered())
             window.location.href = "#registro";
         $('#registro a:eq(0)').tap(capture.takePhoto);
         $('#registro a:eq(1)').tap(fn.registrar);
+        fn.crearReserva();
     },
+//=========================== REGISTRAR ===================
     registrar: function(){
         var nom = $('#registro input:eq(0)').val();
         var mail = $('#registro input:eq(1)').val();
@@ -19,10 +23,52 @@ var fn = {
         else
             navigator.notification.alert('Todos los campos son requeridos', null, 'Error de Datos','Aceptar');
     },
+//=========================== CREAR RESERVA    
     crearReserva: function () {
          // al darle tab a cualquier elemento
-         $('#nr1').tap();
-             
+         
+         var reserva= {
+         	
+         	 selecTH: function(){             // método selecTH
+         	 	if($(this).index()>0){
+                  // de prueba alert($(this).index());          // este index ex el div en donde esta la funcion de reserva en index.html
+                  $('#nr1').attr('th',$(this).index());       // hacer referencia al tipo de habitacion cuando no existe el atributo manda el mansaje de INDEFINIDO
+                  $('#nr1 ul:eq(0) li a').css('background','#f6f6f6');
+                  $(this).find('a').css('background-color','red');      	 		
+         	 	}
+         	 		 	         	 	
+         	 },
+         	 
+//=========================== SIGUIENTE ===================         	 
+         	 siguiente: function () {           // método siguiente
+         	 	// solo fue para prueba  alert($('#nr1').attr('th'));
+         	 	var th=$('#nr1').attr('th');
+         	 	if (th!=undefined && th!='')  // indefinido es porque el usuario no ha seleccionado ningun tipo de habitación
+         	 	    window.location.href='#nr2';
+         	 },
+//=========================== RESERVAR ===================         	 
+         	 reservar: function () {
+         	 	   var th=$('#nr1').attr('th');
+         	 	   var ha=$('#nr2 select:eq(0)').val();
+         	 	   var pr=$('#nr2 select:eq(1)').val();
+         	 	   var di=$('#nr2 select:eq(2)').val();
+         	 	   if (conection.isConnected()) {
+         	 	   	alert();
+         	 	   }  
+         	 	   else {
+         	 	   	almacenamiento.reservar(th,ha,pr,di);
+         	 	   }       	 	            	 	   
+                 //alert("entro a reservar Tipo hab="+th+" Habitaciones = "+ha+" Personas = "+pr+" Dias = "+di);	 	
+              
+         	 }
+         };
+         
+//=========================== EJECUTAR LOS METODOS ===================         
+         $('#nr1 ul:eq(0) li').tap(reserva.selecTH);          //ahora, aqui el ul es la lista en donde estan los tipos de habitacion x lo tanto el index de arriba hará referencia al tipo de habitación
+         $('#nr1 ul:eq(1) li:eq(1)').tap(reserva.siguiente);   // esto es que solo funcione para el boton siguiente eq(0) seria el boton cncelar y eq(1) seria el boton siguiente)
+         $('#nr2 ul:eq(1) li:eq(1)').tap(reserva.reservar);  //Ejecuta el metodo reservar cuando el usuaroi le da click al boton de reservr
+         $('#historial').tap(almacenamiento.leerReservas);  //para que muestre los registros de la base de datos
     }
 };
 $(fn.ready);
+//$(fn.crearReserva);
